57f50c90b6586169f17c1b952559917da2836e1b
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2021-05-12 17:45:40 -0300

    Setup project structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const port = parseInt(process.env.PORT || '8000')
+const app = express();
+
+app.get('/', (_req: Request, res: Response): void => {
+  res.status(200).json({
+    message: 'example application'
+  });
+});
+
+app.listen(port, (): void => {
+  console.info({ message: `Server is running at port ${port}` })
+});
---
+++ test/sample.test.ts
@@ -0,0 +1,7 @@
+import { isMainThread } from "worker_threads";
+
+describe('Sample test suite', () => {
+  it('sample test', () => {
+    expect(1).toBe(1)
+  });
+});
